254f76192a05ec65ac512a1b83ea3f697f58b611
Lucio <LucioMorales>
2020-11-04 16:59:25 -0300

    Models,Admin de Huesped y Habitacion creados

--- config/TopHotel/admin.py
+++ config/TopHotel/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class HuespedAdmin(admin.ModelAdmin):
+    list_display = ['nombre','apellido','dni','telefono']
+    list_display_links = ['nombre','apellido','dni','telefono']
+    fieldsets = (
+        ('Datos',{
+            'fields': ('nombre','apellido','dni','telefono')
+        }),
+    )
+
+class HabitacionAdmin(admin.ModelAdmin):
+    list_display = ['numero','piso']
+    list_display_links = ['numero','piso']
+    fieldsets = (
+        ('Datos',{
+            'fields': ('numero','piso')
+        }),
+    )
+
+admin.site.register(Huesped, HuespedAdmin)
+admin.site.register(Habitacion, HabitacionAdmin)
--- config/TopHotel/models.py
+++ config/TopHotel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Huesped(models.Model):
+    nombre = models.CharField(max_length=35)
+    apellido = models.CharField(max_length=35)
+    dni = models.IntegerField()
+    telefono = models.IntegerField()
+
+    def __str__(self):
+        return "{}".format(self.nombre)
+
+class Habitacion(models.Model):
+    numero = models.IntegerField()
+    piso = models.IntegerField()
+
+    def __str__(self):
+        return "{}".format(self.numero)
